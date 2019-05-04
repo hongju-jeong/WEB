4ff2824b6c8d99cb744bf74bcf0aa707dddf3ec6
muzy <sub06038@naver.com>
2019-05-04 16:37:21 +0900

    js 적용

---
+++ colors.js
@@ -0,0 +1,42 @@
+var Links = {
+  setColor: function(color)
+  {
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i<alist.length)
+    {
+      alist[i].style.color= color;
+      i++;
+    }
+  }
+}
+var Body = {
+  setColor: function(color)
+  {
+    document.querySelector('body').style.color =color ;
+  },
+  setBackgroundColor: function(color)
+  {
+    document.querySelector('body').style.backgroundColor = color ;
+
+  }
+}
+
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value === 'night')
+  {
+    Body.setBackgroundColor('black');
+    self.value = 'day';
+    Body.setColor('white');
+
+    Links.setColor('white');
+  }
+  else
+  {
+    Body.setBackgroundColor('white');
+    self.value = 'night';
+    Body.setColor('black');
+    Links.setColor('blue');
+  }
+}
